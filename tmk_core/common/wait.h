741ad50a7168623a2317a3598bd1b53ca33f8ceb
skullY <skullydazed@gmail.com>
2019-10-22 20:48:12 -0700

    Revert "first attempt at fixing wait_us() on chibios"
    This reverts commit b2fea920902beb2329f94e57d1eecd6a97f25799.

--- tmk_core/common/wait.h
+++ tmk_core/common/wait.h
@@ -21,7 +21,14 @@ extern "C" {
                 chThdSleepMicroseconds(1);  \
             }                               \
         } while (0)
-void wait_us(uint64_t us);
+#    define wait_us(us)                     \
+        do {                                \
+            if (us != 0) {                  \
+                chThdSleepMicroseconds(us); \
+            } else {                        \
+                chThdSleepMicroseconds(1);  \
+            }                               \
+        } while (0)
 #elif defined PROTOCOL_ARM_ATSAM
 #    include "clks.h"
 #    define wait_ms(ms) CLK_delay_ms(ms)
